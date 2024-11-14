649f891193e7ee8bebee0c0c02639296e53730cb
GustavAAK <gustavgodsk@gmail.com>
2024-11-14 11:39:23 +0100

    dsfghjkl

---
+++ data.js
@@ -0,0 +1,76 @@
+window.roots = [
+  {
+    "rootName": "Aarhus Ø",
+    "primaryColor": "#004050",
+    "primaryColorAlpha": "rgba(0, 64, 80, 0.4)",
+    "areas": [
+      {
+        "areaName": "Pier 4",
+        "oversigtskort": "",
+        "elementer": [
+          {
+            "id": 1731580143381,
+            "navn": "Sejlsportscentret",
+            "beskrivelse": "",
+            "billeder": {
+              "primary": "3.jpg",
+              "all": [
+                "1.jpg",
+                "3.jpg",
+                "2.jpg"
+              ],
+              "metadata": {
+                "1.jpg": {
+                  "description": "Beskrivelse",
+                  "author": "eeeee",
+                  "year": "2121"
+                },
+                "3.jpg": {
+                  "description": "dawd",
+                  "author": "",
+                  "year": "2222"
+                },
+                "2.jpg": {
+                  "description": "",
+                  "author": "dwa",
+                  "year": ""
+                }
+              }
+            },
+            "faktaboks": {
+              "igangsat": "",
+              "færdigt": "",
+              "boliger": "",
+              "erhvervsareal": "",
+              "typologi": "",
+              "etager": "",
+              "bygherre": "",
+              "arkitekt": ""
+            },
+            "ekstraInfo": [],
+            "infoCirkelPlacering": "rightTop",
+            "faktaboksPlacering": "leftBottom",
+            "x": "50%",
+            "y": "50%"
+          }
+        ],
+        "primaryColor": "#004050",
+        "primaryColorAlpha": "rgba(0, 64, 80, 0.4)"
+      }
+    ]
+  },
+  {
+    "rootName": "Amtssygehuset",
+    "primaryColor": "#004050",
+    "primaryColorAlpha": "rgba(0, 64, 80, 0.4)",
+    "areas": [
+      {
+        "areaName": "Oversigt",
+        "oversigtskort": "",
+        "elementer": [],
+        "primaryColor": "#004050",
+        "primaryColorAlpha": "rgba(0, 64, 80, 0.4)"
+      }
+    ]
+  }
+]
